658f2df9caa5e2d3ade385e946bc38aa23f9721c
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-20 12:56:33 -0600

    Updated to latest Angular
    Issues trying to get this to build and manually fixing it, so created a new set of project files with ng new and copied over the files from the course. Updated Http to HttpClient.

---
+++ src/app/06-services/todo.service.ts
@@ -0,0 +1,20 @@
+
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+
+export class TodoService { 
+  constructor(private http: HttpClient) { 
+  }
+
+  add(todo) {
+    return this.http.post('...', todo);
+  }
+
+  getTodos() { 
+    return this.http.get('...');
+  }
+
+  delete(id) {
+    return this.http.delete('...');
+  }
+}
