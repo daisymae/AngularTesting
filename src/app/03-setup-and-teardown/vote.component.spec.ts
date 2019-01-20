10028c074867ddc88229a6f55319a08c791cb6b6
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-20 13:42:41 -0600

    component test
    First attempt at testing a component.

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,9 +1,20 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('', () => {
+  it('should increment totalVotes when upvoted', () => {
+    // Arrange -- initialize system under test
+    let component = new VoteComponent();
+    // Act -- call method or function
+    component.upVote();
+    // Assert
+    expect (component.totalVotes).toBe(1);
+  });
+  it('should decrement totalVotes when downvoted', () => {
+    let component = new VoteComponent();
+
+    component.downVote();
+
+    expect (component.totalVotes).toBe(-1);
   });
 
-  it('', () => {
-  });
 });
