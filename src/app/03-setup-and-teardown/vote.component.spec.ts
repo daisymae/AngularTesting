2632d82ef54bd794e533b66120f69e87933f1df3
daisymae <daisymae.orcadiafarm@gmail.com>
2019-01-20 13:47:13 -0600

    Update vote.component.spec.ts
    Reorganize code to remove duplication. Document other functions available for set up and tear down.

--- src/app/03-setup-and-teardown/vote.component.spec.ts
+++ src/app/03-setup-and-teardown/vote.component.spec.ts
@@ -1,17 +1,28 @@
 import { VoteComponent } from './vote.component'; 
 
 describe('VoteComponent', () => {
-  it('should increment totalVotes when upvoted', () => {
     // Arrange -- initialize system under test
-    let component = new VoteComponent();
+    let component : VoteComponent;
+    // puting initialization in the suite can save on code, but might also cause
+    // side effects as one test may change expected values for another.
+    beforeEach(() => {
+      // set up
+      // instead put initialization here
+      component = new VoteComponent();
+    })
+
+    afterEach(() => {}) // available for any cleanup needed
+    beforeAll(()=> {}) // executed once before ALL tests
+    afterAll(()=>{}) // executed once after ALL tests
+
+  it('should increment totalVotes when upvoted', () => {
     // Act -- call method or function
     component.upVote();
     // Assert
     expect (component.totalVotes).toBe(1);
   });
-  it('should decrement totalVotes when downvoted', () => {
-    let component = new VoteComponent();
 
+  it('should decrement totalVotes when downvoted', () => {
     component.downVote();
 
     expect (component.totalVotes).toBe(-1);
